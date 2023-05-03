77546b55451c70df6f6cec6a9a7cee62664c4e32
KseniaSolov <ksenivv@gmai.com>
2023-05-03 13:13:59 +0300

    4.6 practice. solving problem
    Payroll

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-console.log("Hellow world");
+//Первое решение
 
+/*const salary = 80;
+const workTime = 5;
+const workDays = 11 - 11 / 7;
+console.log(40 <= workTime * workDays) 
+console.log(salary * 40)
+*/
+
+
+//Второе решение
+
+/*const salary = 80;
+const workTime = 5 * 5;
+const timeHours = 40;
+console.log(11 / 7 * 40 >= timeHours / workTime)
+console.log(timeHours * salary)
+*/
+
+
+//Решение после разбора
+
+const payRate = 80;
+const projectTime = 40;
+const freeTime = 5 * 9; 
+console.log("Могу ли взяться за работу? " + (projectTime <= freeTime));
+console.log("Работа будет стоить: " + (payRate * projectTime) + "$")

9e0a7b5cc87b19df4cdaf3b58caba3c593ba71e6
KseniaSolov <ksenivv@gmai.com>
2023-05-08 13:52:24 +0300

    5.4 topic  "switch"
    training with swith

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-/*
-Вася положил 12 000 на вклад 7% годовых с капитализацией 1 раз в месяц. Вывести в консоль, сможет ли он купить дом за 13 500 через 2 года после снятия вклада. И остаток после покупки.
-Итог = Сумма * (1 + Ставка в месяц не в %) ** срок в месяцах
-*/
+const role = "admin";
 
-const deposit = 12000;
-const time = 24;
-const rate = 0.07;
-const goal = 13500;
-let remains;
-const result = deposit * (1 + rate / 12) ** time;
-if (result >= goal) {
-    console.log(`Вы накопили: ${result}.
-Денег на квартиру хватает.
-Осталось: ${result - 13500}`)
-} else {
-    console.log(`Вы накопили: ${result}.
-Денег на квартиру не хватает`)
+switch (role) {
+    case "manager":
+        console.log("Менеджер");
+        break;
+    case "admin":
+        console.log("Администратор");
+        break;
+    case "worker":
+        console.log("Работник");
+        break;
+    default:
+        console.log("Доступ запрещен")
+        break;
 }

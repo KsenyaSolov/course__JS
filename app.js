5017947ed3629b8cdd8cf790428b46df101e52fe
KseniaSolov <ksenivv@gmai.com>
2023-05-11 19:28:02 +0300

    7.8 My working. Credit acces
    problem solution

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const problem = prompt("Сколько будет 7 + 15?");
-switch (problem) {
-    case "22":
-    case "Я не робот":
-        console.log("Успех");
-        break;
-    default: 
-        console.log("Вы робот!");
+const money = prompt("Какая сумма денег у вас имеется?");
+const age = prompt("Сколько Вам лет?");
+let work = prompt("Вы работаете")
+
+if((work === "Да") || (work === "да")) {
+    work = true;
 }
+
+
+function creditSum(age, work) {
+    if((age >= 24) & (work == true)) {
+            return 500;
+    } else if(age >= 24) {
+            return 100;
+    } else { 
+        return 0;
+        }
+}
+
+console.log(creditSum(24, true));
+
+function result(money, creditSum) {
+    if(money + creditSum(age, work) >= 2000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(result(money, c))

e47332f9c4f4e280b1b845d2f29c00ff6edcb68c
KseniaSolov <ksenivv@gmai.com>
2023-05-27 12:38:43 +0300

    7.8 My working solution

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
-const money = prompt("Какая сумма денег у вас имеется?");
-const age = prompt("Сколько Вам лет?");
-let work = prompt("Вы работаете")
-
-if((work === "Да") || (work === "да")) {
-    work = true;
-}
-
-
-function creditSum(age, work) {
-    if((age >= 24) & (work == true)) {
-            return 500;
-    } else if(age >= 24) {
-            return 100;
-    } else { 
+function calculateCreditSum(age, work) {
+    if (age > 24 && work === true){
+        return 500;
+    }
+    else if(age > 24 && work === false) {
+        return 100;
+    }
+    else {
         return 0;
-        }
-}
-
-console.log(creditSum(24, true));
-
-function result(money, creditSum) {
-    if(money + creditSum(age, work) >= 2000) {
-        return true;
-    } else {
-        return false;
     }
 }
 
-console.log(result(money, c))
+function isEnoughMoney(money, age, work) {
+    const creditSum = calculateCreditSum(age, work)
+    if((money + creditSum) >= 2000) {
+        return true;
+    }
+    return false;
+}
+
+
+
+console.log(isEnoughMoney(1500, 25, true))
+
